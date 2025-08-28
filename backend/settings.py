6b28003a3d47a8f0fe11c7d0a2e3260c8e018035
riyazuddin-1 <riyazuddin9846@gmail.com>
2025-08-28 22:35:46 +0530

    Debug state set to False for production.

--- backend/settings.py
+++ backend/settings.py
@@ -7,7 +7,7 @@ load_dotenv()
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('SECRET_KEY', 'vadavowvow')
-DEBUG = os.getenv('DEBUG', True)
+DEBUG = False
 ALLOWED_HOSTS = ['.vercel.app']
 
 
