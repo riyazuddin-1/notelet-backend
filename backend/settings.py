4c2c9328324c0e75973373e9dca4e2236d12c37c
riyazuddin-1 <riyazuddin9846@gmail.com>
2025-08-28 22:21:11 +0530

    updated requirements.txt, added 'dj-database-url'

--- backend/settings.py
+++ backend/settings.py
@@ -8,7 +8,7 @@ load_dotenv()
 BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('SECRET_KEY', 'vadavowvow')
 DEBUG = os.getenv('DEBUG', True)
-ALLOWED_HOSTS = ['.vercel.app', '']
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
